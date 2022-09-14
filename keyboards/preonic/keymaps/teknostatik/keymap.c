cfd1cdb976d932ab6814ff3f8a6a784296a24c4b
Andy Ferguson <andy@teknostatik.org>
2022-09-14 11:55:29 +0100

    Adding navigation

--- keyboards/preonic/keymaps/teknostatik/keymap.c
+++ keyboards/preonic/keymaps/teknostatik/keymap.c
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,
   MT(MOD_LCTL, KC_NUHS),  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   MT(MOD_LSFT, KC_NUBS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, MT(MOD_RSFT, KC_ENT),
-MT(MOD_LCTL, KC_F5), KC_LCTL, MT(MOD_LGUI, KC_PAST), KC_LALT,  LOWER, KC_SPC, KC_SPC, RAISE, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
+MT(MOD_LCTL, KC_F5), KC_LCTL, MT(MOD_LGUI, KC_PAST), KC_LALT,  LOWER, KC_SPC, KC_SPC, RAISE, MT(MOD_LALT, KC_LEFT), LAG_T(KC_DOWN), MT(MOD_LCTL, KC_UP), MT(MOD_LCTL, KC_RGHT)
 ),
 
 /* Colemak
@@ -124,7 +124,7 @@ MT(MOD_LCTL, KC_F5), KC_LCTL, MT(MOD_LGUI, KC_PAST), KC_LALT,  LOWER, KC_SPC, KC
 /* Raise
  * ,-----------------------------------------------------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Bksp |
- * |------+------+------+------+------+------+------+------+------+------+------+------|
+ * |------+------+------+------+------+------+------+------+------+------+------+------|!!!!!!!!!!!!!!!!!1
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Del  |  F1  |  F2  |  F3  |  F4  |  F5  |  F6  |   -  |   =  |   [  |   ]  |  \   |
@@ -135,10 +135,10 @@ MT(MOD_LCTL, KC_F5), KC_LCTL, MT(MOD_LGUI, KC_PAST), KC_LALT,  LOWER, KC_SPC, KC
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_preonic_grid(
-  KC_GRV,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F0,    LCA(KC_DEL),
+  KC_GRV,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,    LCA(KC_DEL),
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_PSCR, KC_DEL,
   KC_DEL,  KC_4,   KC_5,   KC_6,   KC_F4,   KC_F5,   KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,
-  _______, KC_7,   KC_8,   KC_9,   KC_10,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
+  _______, KC_7,   KC_8,   KC_9,   KC_0,  KC_F11,  KC_F12,  KC_NUHS, KC_NUBS, KC_PGUP, KC_PGDN, _______,
   _______, _______, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END
 ),
 
